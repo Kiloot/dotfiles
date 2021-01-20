db706dc4f2d3b057ca868c7dda4dca2fe3f85c29
Pierre Frelot <pierrotfrelot@gmail.com>
2021-01-20 12:06:34 +0100

    Add configuration files

---
+++ .config/emacs/init.el
@@ -0,0 +1,21 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(unless package--initialized (package-initialize))
+
+(require 'org)
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "initpft.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(nord-theme syntax-subword use-package projectile iedit diminish counsel ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
