62cd063af87d1af5351cf653de0e13b263a060c1
Pierre Frelot <pierrotfrelot@gmail.com>
2021-01-25 14:15:20 +0100

    Update things

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -12,7 +12,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(markdown-mode doom-themes nord-theme syntax-subword use-package projectile iedit diminish counsel ace-window)))
+   '(pdf-tools flx markdown-mode doom-themes nord-theme syntax-subword use-package projectile iedit diminish counsel ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
