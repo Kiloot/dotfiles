211f320017288707225fd3e3c804c3d89a9fc268
Pierre Frelot <pierrotfrelot@gmail.com>
2021-04-04 19:49:22 +0200

    Update emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -11,8 +11,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(completion-styles '(orderless) nil nil "Customized with use-package orderless")
+ '(custom-safe-themes
+   '("77113617a0642d74767295c4408e17da3bfd9aa80aaa2b4eeb34680f6172d71a" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
  '(package-selected-packages
-   '(pdf-tools flx markdown-mode doom-themes nord-theme syntax-subword use-package projectile iedit diminish counsel ace-window)))
+   '(doom-theme cider adoc-mode which-key marginalia embark-consult selectrum-prescient selectrum pdf-tools markdown-mode doom-themes nord-theme syntax-subword use-package projectile iedit diminish ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
